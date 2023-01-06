84e114601c8ed451c894a30a361f7ebcb4d28b66
agn-wita <agn.wita@gmail.com>
2023-01-06 19:32:56 +0100

    main: Added increment and save functions

---
+++ index.js
@@ -0,0 +1,15 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count=0
+console.log(saveEl)
+
+function increment() {
+    // count = count + 1
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    countStr = count + " - "
+    saveEl.innerText += countStr
+}

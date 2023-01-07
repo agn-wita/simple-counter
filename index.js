a7be8cb65f29c7ae026f12b98fefbb6d14b11ecf
agn-wita <agn.wita@gmail.com>
2023-01-07 17:53:07 +0100

    main: Set count to 0

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count=0
-console.log(saveEl)
 
 function increment() {
     // count = count + 1
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     countStr = count + " - "
-    saveEl.innerContent += countStr
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
